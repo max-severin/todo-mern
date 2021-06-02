66313373ecd6ccead4863f0790b9540ece79b0c2
Max Severin <makc.severin@gmail.com>
2021-06-02 04:48:51 +0300

    Add Task Schema

---
+++ server/models/task.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+const { Schema } = mongoose;
+
+const taskSchema = new Schema({
+  title: { type: String, required: true },
+  description: { type: String },
+}, { 
+  timestamps: true 
+});
+
+module.exports = mongoose.model('task', taskSchema);
