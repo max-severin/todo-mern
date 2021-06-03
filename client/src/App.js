dedce9d11ffad407074c80a742567c04dbeca4ea
Max Severin <makc.severin@gmail.com>
2021-06-03 03:57:18 +0300

    Fetch tasks from API

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,29 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 import './App.css';
 import TaskForm from './components/TaskForm';
+import TaskList from './components/TaskList';
+
+const API_URL = process.env.REACT_APP_API_URL;
 
 function App() {
+  const [tasks, setTasks] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  const getTasks = async () => {
+    try {
+      const asyncRes = await axios(`${API_URL}/tasks`);
+
+      setTasks(asyncRes.data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    getTasks();
+  }, []);
+
   return (
     <div className="todo-app">
       <header className="todo-app-header">
@@ -11,6 +33,7 @@ function App() {
       </header>
       <main>
         <TaskForm createTask={alert} />
+        <TaskList tasks={tasks} updateTask={alert} deleteTask={alert} />
       </main>
     </div>
   );
