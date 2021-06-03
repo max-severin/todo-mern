253fb2f739d3c31cda226cbc2c6c0202cf447e55
Max Severin <makc.severin@gmail.com>
2021-06-03 03:56:15 +0300

    Create TaskList component

---
+++ client/src/components/TaskList.js
@@ -0,0 +1,44 @@
+/* eslint-disable no-underscore-dangle */
+import { useState } from 'react';
+import PropTypes from 'prop-types';
+
+const TaskList = ({ tasks, updateTask, deleteTask }) => {
+  const [title, setTitle] = useState('');
+  const [description, setDescription] = useState('');
+
+  return (
+    <ul>
+      {tasks.map((task) => (
+        <li key={task._id}>
+          <h3>{task.title}</h3>
+          <p>{task.description}</p>
+          <input
+            type="button"
+            value="Delete"
+            onClick={(event) => {
+              deleteTask(task._id);
+            }}
+          />
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+TaskList.propTypes = {
+  tasks: PropTypes.arrayOf(
+    PropTypes.shape({
+      _id: PropTypes.string.isRequired,
+      title: PropTypes.string.isRequired,
+      description: PropTypes.string,
+    })
+  ),
+  updateTask: PropTypes.func.isRequired,
+  deleteTask: PropTypes.func.isRequired,
+};
+
+TaskList.defaultProps = {
+  tasks: [],
+};
+
+export default TaskList;
