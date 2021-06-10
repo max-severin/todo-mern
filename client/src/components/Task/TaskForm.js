d9ae73a675b03987a6378a8cf7dfc1b3e01b55a8
Max Severin <makc.severin@gmail.com>
2021-06-10 22:49:34 +0300

    Add TaskForm styles

--- client/src/components/Task/TaskForm.js
+++ client/src/components/Task/TaskForm.js
@@ -1,25 +1,28 @@
 import { useState } from 'react';
 import PropTypes from 'prop-types';
+import './TaskForm.scss';
 
 const TaskForm = ({ createTask }) => {
   const [title, setTitle] = useState('');
   const [description, setDescription] = useState('');
 
   return (
-    <form onSubmit={(event) => {
-      event.preventDefault();
+    <form
+      className="task-create-form"
+      onSubmit={(event) => {
+        event.preventDefault();
 
-      createTask({ title, description });
+        createTask({ title, description });
 
-      setTitle('');
-      setDescription('');
-    }}
+        setTitle('');
+        setDescription('');
+      }}
     >
       <input type="text" name="title" placeholder="Title" value={title} onChange={(e) => setTitle(e.target.value)} />
 
       <textarea name="description" placeholder="Description" value={description} onChange={(e) => setDescription(e.target.value)} />
 
-      <input type="submit" />
+      <input type="submit" value="Add" />
     </form>
   );
 };
